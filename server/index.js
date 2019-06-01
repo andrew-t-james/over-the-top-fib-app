40944e0cafa3445973d4442bc277da1606d55cb5
Andrew James <ajames20@gmail.com>
2019-06-01 18:00:35 -0500

    Add redis config

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
+/* eslint-disable no-console */
 // Express set up
 const express = require('express');
 const bodyParser = require('body-parse');
 const cors = require('cors');
+const { Pool } = require('pg');
+const redis = require('redis');
 
 const keys = require('./keys');
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Postgres config
-
-const { Pool } = require('pg');
-
 const pgClient = new Pool({
   user: keys.pgUser,
   hose: keys.pgHost,
@@ -26,3 +27,12 @@ pgClient.on('error', () => console.log('Pg Client lost connection'));
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values(number INT)')
   .catch(error => console.log(error));
+
+// Redis config
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
