a55d8788d8fab219e298e9f286058f35ec19295f
Andrew James <ajames20@gmail.com>
2019-06-04 06:53:11 -0500

    Move to same keys config

--- worker/index.js
+++ worker/index.js
@@ -1,15 +1,13 @@
 const redis = require('redis');
-const { redisHost, redisPort } = require('./keys');
+const keys = require('./keys');
 
 const redisClient = redis.createClient({
-  host: redisHost,
-  port: redisPort,
+  host: keys.redisHost,
+  port: keys.redisPort,
   retry_strategy: () => 1000
 });
-
 const sub = redisClient.duplicate();
 
-// using recursion on purpose for slow processing
 function fib(index) {
   if (index < 2) return 1;
   return fib(index - 1) + fib(index - 2);
@@ -19,5 +17,4 @@ sub.on('message', (channel, message) => {
   // eslint-disable-next-line radix
   redisClient.hset('values', message, fib(parseInt(message)));
 });
-
 sub.subscribe('insert');
